3413c6e7aeae722e9fba0784bea580bb2de173e1
Donatien Rolland <donatien@rollandmail.com>
2018-09-23 20:18:15 +0700

    solve

--- app/models/user_activity.rb
+++ app/models/user_activity.rb
@@ -1,8 +1,8 @@
 class UserActivity < ApplicationRecord
-  belongs_to :user, dependent: :destroy
+  belongs_to :user
   accepts_nested_attributes_for :user, :reject_if => :all_blank
 
-  belongs_to :activity, dependent: :destroy
+  belongs_to :activity
   accepts_nested_attributes_for :activity, :reject_if => :all_blank
 
   scope :by_activity_title, -> (current_title) { joins(:activity).merge(Activity.by_title(current_title)) }
