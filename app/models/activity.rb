63fb7d2d04a5041d6bb9aa7c816443ab933b4c4c
Donatien Rolland <donatien@rollandmail.com>
2018-09-23 20:39:35 +0700

    done

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,5 +1,5 @@
 class Activity < ApplicationRecord
-  belongs_to :category, dependent: :destroy
+  belongs_to :category
 
   has_many :user_activity, dependent: :destroy
   has_many :users, through: :user_activities
